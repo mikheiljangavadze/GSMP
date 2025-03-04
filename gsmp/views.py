dc2a5054e42b48343978989ebb41d8940da1adb4
mikheiljangavadze <mikheil.jangavadze@tsu.ge>
2025-03-04 20:02:59 +0400

    afeter PDF commit

--- gsmp/views.py
+++ gsmp/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template.loader import render_to_string
+from django.urls import path
 
 from django.utils.translation import gettext as _
 from django.utils.translation import get_language, activate, gettext
@@ -20,6 +21,7 @@ from django.shortcuts import get_object_or_404
 
 
 from gsmp.models import Article  # შეცვალე შენი მოდელის სახელით
+
 
 
 # @login_required
@@ -47,79 +49,76 @@ def about(request):
 
 def mission(request):
     article = get_object_or_404(Article, slug="WhoWeAre")
-
     context = {
         'article': article,
     }
-
     return render(request, 'gsmp/mission.html', context=context)
-
-
 
 
 def news(request):
 
-    return render(request, 'gsmp/index.html',
-                )
+    return render(request, 'gsmp/index.html',)
 
 
 def events(request):
-
-    return render(request, 'gsmp/index.html',
-                  )
+    return render(request, 'gsmp/index.html',)
 
 
 def resources(request):
-
-    return render(request, 'gsmp/resources.html',
-                   )
+    return render(request, 'gsmp/resources.html',)
 
 
 def members(request):
-
-    return render(request, 'gsmp/memberregistrationform.html',
-                   )
+    return render(request, 'gsmp/memberregistrationform.html',)
 
 
 
 
 def director_board(request):
     article = get_object_or_404(Article, slug="WhoWeAre")
-
     context = {
         'article': article,
     }
-
     return render(request, 'gsmp/director_board.html', context=context)
 
 def advisory_board (request):
     article = get_object_or_404(Article, slug="WhoWeAre")
-
     context = {
         'article': article,
     }
-
     return render(request, 'gsmp/advisory_board.html', context=context)
 
 
 def who_we_are(request):
-    article = get_object_or_404(Article, slug="who_we_are")
 
+
+
+    article = get_object_or_404(Article, slug="who_we_are")
     context = {
         'article': article,
     }
-
     return render(request, 'gsmp/who_we_are.html', context=context)
 
 
+# def articlaaaa(request):
+#
+#
+#     # slug = slug.split("/").pop(-2)
+#
+#
+#
+#     article = get_object_or_404(Article, slug="slug")
+#     context = {
+#         'article': article,
+#     }
+#     return render(request, 'gsmp/who_we_are.html', context=context)
+#
 
 def history(request):
     article = get_object_or_404(Article, slug="who_we_are")
-
     context = {
         'article': article,
     }
-
     return render(request, 'gsmp/history.html', context=context)
 
 
